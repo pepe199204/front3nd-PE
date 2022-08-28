071516dd64e52ffdaed195f163333e2398978684
pepe199204 <pepe199204@gmail.com>
2022-08-27 21:11:28 -0500

    entregable finalizado con lo solicitado por el docente, listo para enviar

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,4 @@
-
+import React, { useState } from "react"
 // El componente Item no tiene componentes hijos.
 // ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
 // MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
@@ -9,11 +9,26 @@
 //    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
 //    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
 
-export default function Item() {
+export default function Item(props) {
 
+  const {nombre,descripcion,stock,contadorItem} = props;
+  //console.log(stock);
+  let totalStock = 0;
+  const [countStock,setStock] = useState(stock);
+  const validacionStock = countStock <= 0;
+  const handleStock = () => {
+    setStock(countStock - 1);
+    contadorItem(totalStock + 1);
+    
+  }
   return (
     <div className='producto'>
+      
       {/* maquetar Item aquí */}
+      <h3>{nombre}</h3>
+      <p>{descripcion}</p>
+      <h5>En Stock: {validacionStock ? <span>Agotado</span> : countStock} </h5>
+      <button onClick={() => handleStock()} disabled={validacionStock ? true : false} id="shop">{validacionStock ? 'SIN STOCK' : 'COMPRAR'}</button>
     </div>
   )
 }
